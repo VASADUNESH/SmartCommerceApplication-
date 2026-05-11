65b5df939ce5fddab5b2bf33cf1ed0457d2b65f2
VASADUNESH <duneshvasa@gmail.com>
2026-05-11 14:59:10 +0530

    feat: updated Swagger doc

--- src/main/java/com/smartCommerce/smart_commerce/config/SwaggerConfig.java
+++ src/main/java/com/smartCommerce/smart_commerce/config/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package com.smartCommerce.smart_commerce.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 
+@Configuration
 public class SwaggerConfig {
 
+	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
 				.info(new Info()
